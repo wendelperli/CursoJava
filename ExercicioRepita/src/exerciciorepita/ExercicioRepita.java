f4a67d5a314c83b6b83a8ff3e7da7885667f9289
wendelperli <140542316+wendelperli@users.noreply.github.com>
2024-07-23 16:23:14 -0300

    Add files via upload

---
+++ ExercicioRepita/src/exerciciorepita/ExercicioRepita.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exerciciorepita;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Allan
+ */
+public class ExercicioRepita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //JOptionPane.showMessageDialog(null, "Olá, mundo!", "Boas-vindas", JOptionPane.WARNING_MESSAGE);
+        //JOptionPane.showMessageDialog(null, "Você digitou o valor " + n);
+        int n, s = 0;
+        do{
+        n = Integer.parseInt(JOptionPane.showInputDialog(null, 
+                "<html>Informe um número: <br><em>(valor 0 interrompe)</em</html>"));
+        s += n;
+        }while(n != 0);
+        JOptionPane.showMessageDialog(null, "<html>Resultado final <br>-------------" + 
+                "<br>Somatório vale " + s + "</html>");
+    }
+    
+}
